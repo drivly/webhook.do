3a113b806159c0019e2675c7068886a6531fdede
Nathan Clevenger <nateclev@gmail.com>
2022-09-13 07:23:14 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -14,15 +14,15 @@ const api = {
 
 export default {
   fetch: async (req, env) => {
-    const { user, body, url, headers, cf } = await env.CTX.fetch(req).then(res => res.json())
+    const { user, body, url, time, headers, cf } = await env.CTX.fetch(req).then(res => res.json())
     const { origin, hostname, pathname } = new URL(req.url)
     const [ _, namespace, id = headers['cf-ray'] ] = pathname.split('/')
     const ua = headers['user-agent']
     const { ip, isp, city, region, country, continent } = user
     const location = `${city}, ${region}, ${country}, ${continent}`
     const list = `https://webhooks.do/${namespace}`
-    const data = body ? await env.WEBHOOKS.put(`${namespace}/${id}`, JSON.stringify({ namespace, id, url, body, headers, cf, user }, null, 2) , { 
-      metadata: { ip, ua, location, url: `https://webhooks.do/${namespace}/${id}` },
+    const data = body ? await env.WEBHOOKS.put(`${namespace}/${id}`, JSON.stringify({ namespace, id, time, url, body, headers, cf, user }, null, 2) , { 
+      metadata: { time, ip, ua, location, url: `https://webhooks.do/${namespace}/${id}` },
       expirationTtl: 30 * 24 * 60 * 60 ,
     }) : id != headers['cf-ray'] ? await env.WEBHOOKS.get(`${namespace}/${id}`, { type: "json" }) : await env.WEBHOOKS.list({ prefix: `${namespace}/`}).then(list => list.keys.map(item => item.metadata))
     return new Response(JSON.stringify({ api, namespace, id, list, data, user }, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }})
